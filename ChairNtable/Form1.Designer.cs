99d1eb5361225e44fe7e37cee69ddd835c16274b
Tauqeer <tauqeerkethwal@gmail.com>
2017-07-13 00:28:54 +0200

    Eyeshot library

---
+++ ChairNtable/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ChairNtable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            devDept.Eyeshot.CancelToolBarButton cancelToolBarButton1 = new devDept.Eyeshot.CancelToolBarButton("Cancel", devDept.Eyeshot.ToolBarButton.styleType.ToggleButton, true, true);
+            devDept.Eyeshot.ProgressBar progressBar1 = new devDept.Eyeshot.ProgressBar(devDept.Eyeshot.ProgressBar.styleType.CircularWin10, 0, "Idle", System.Drawing.Color.Black, System.Drawing.Color.Transparent, System.Drawing.Color.Green, 1D, true, cancelToolBarButton1, false);
+            devDept.Graphics.BackgroundSettings backgroundSettings1 = new devDept.Graphics.BackgroundSettings(devDept.Graphics.backgroundStyleType.LinearGradient, System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(231))))), System.Drawing.Color.DodgerBlue, System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(231)))), ((int)(((byte)(231))))), 0.75D, null, devDept.Graphics.colorThemeType.Auto, 0.3D);
+            devDept.Eyeshot.Camera camera1 = new devDept.Eyeshot.Camera(new devDept.Geometry.Point3D(0D, 0D, 50D), 600D, new devDept.Geometry.Quaternion(0.086824088833465166D, 0.15038373318043533D, 0.492403876506104D, 0.85286853195244339D), devDept.Graphics.projectionType.Perspective, 50D, 7.2600007097286721D, false, 0.001D);
+            devDept.Eyeshot.HomeToolBarButton homeToolBarButton1 = new devDept.Eyeshot.HomeToolBarButton("Home", devDept.Eyeshot.ToolBarButton.styleType.PushButton, true, true);
+            devDept.Eyeshot.MagnifyingGlassToolBarButton magnifyingGlassToolBarButton1 = new devDept.Eyeshot.MagnifyingGlassToolBarButton("Magnifying Glass", devDept.Eyeshot.ToolBarButton.styleType.ToggleButton, true, true);
+            devDept.Eyeshot.ZoomWindowToolBarButton zoomWindowToolBarButton1 = new devDept.Eyeshot.ZoomWindowToolBarButton("Zoom Window", devDept.Eyeshot.ToolBarButton.styleType.ToggleButton, true, true);
+            devDept.Eyeshot.ZoomToolBarButton zoomToolBarButton1 = new devDept.Eyeshot.ZoomToolBarButton("Zoom", devDept.Eyeshot.ToolBarButton.styleType.ToggleButton, true, true);
+            devDept.Eyeshot.PanToolBarButton panToolBarButton1 = new devDept.Eyeshot.PanToolBarButton("Pan", devDept.Eyeshot.ToolBarButton.styleType.ToggleButton, true, true);
+            devDept.Eyeshot.RotateToolBarButton rotateToolBarButton1 = new devDept.Eyeshot.RotateToolBarButton("Rotate", devDept.Eyeshot.ToolBarButton.styleType.ToggleButton, true, true);
+            devDept.Eyeshot.ZoomFitToolBarButton zoomFitToolBarButton1 = new devDept.Eyeshot.ZoomFitToolBarButton("Zoom Fit", devDept.Eyeshot.ToolBarButton.styleType.PushButton, true, true);
+            devDept.Eyeshot.ToolBar toolBar1 = new devDept.Eyeshot.ToolBar(devDept.Eyeshot.ToolBar.positionType.HorizontalTopCenter, true, new devDept.Eyeshot.ToolBarButton[] {
+            ((devDept.Eyeshot.ToolBarButton)(homeToolBarButton1)),
+            ((devDept.Eyeshot.ToolBarButton)(magnifyingGlassToolBarButton1)),
+            ((devDept.Eyeshot.ToolBarButton)(zoomWindowToolBarButton1)),
+            ((devDept.Eyeshot.ToolBarButton)(zoomToolBarButton1)),
+            ((devDept.Eyeshot.ToolBarButton)(panToolBarButton1)),
+            ((devDept.Eyeshot.ToolBarButton)(rotateToolBarButton1)),
+            ((devDept.Eyeshot.ToolBarButton)(zoomFitToolBarButton1))});
+            devDept.Eyeshot.Grid grid1 = new devDept.Eyeshot.Grid(new devDept.Geometry.Point3D(-100D, -100D, 0D), new devDept.Geometry.Point3D(100D, 100D, 0D), 5D, new devDept.Geometry.Plane(new devDept.Geometry.Point3D(0D, 0D, 0D), new devDept.Geometry.Vector3D(0D, 0D, 1D)), System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128))))), System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32))))), System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32))))), false, true, false, false, 10, 100, 5, System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(90)))), ((int)(((byte)(90)))), ((int)(((byte)(90))))), System.Drawing.Color.Transparent, false);
+            devDept.Eyeshot.RotateSettings rotateSettings1 = new devDept.Eyeshot.RotateSettings(new devDept.Eyeshot.MouseButton(devDept.Eyeshot.mouseButtonsZPR.Middle, devDept.Eyeshot.modifierKeys.None), 10D, true, 1D, devDept.Eyeshot.rotationType.Trackball, devDept.Eyeshot.rotationCenterType.CursorLocation, new devDept.Geometry.Point3D(0D, 0D, 0D), false);
+            devDept.Eyeshot.ZoomSettings zoomSettings1 = new devDept.Eyeshot.ZoomSettings(new devDept.Eyeshot.MouseButton(devDept.Eyeshot.mouseButtonsZPR.Middle, devDept.Eyeshot.modifierKeys.Shift), 25, true, devDept.Eyeshot.zoomStyleType.AtCursorLocation, false, 1D, System.Drawing.Color.Empty, devDept.Eyeshot.Camera.perspectiveFitType.Accurate, true, 10, true);
+            devDept.Eyeshot.PanSettings panSettings1 = new devDept.Eyeshot.PanSettings(new devDept.Eyeshot.MouseButton(devDept.Eyeshot.mouseButtonsZPR.Middle, devDept.Eyeshot.modifierKeys.Ctrl), 25, true);
+            devDept.Eyeshot.NavigationSettings navigationSettings1 = new devDept.Eyeshot.NavigationSettings(devDept.Eyeshot.Camera.navigationType.Examine, new devDept.Eyeshot.MouseButton(devDept.Eyeshot.mouseButtonsZPR.Left, devDept.Eyeshot.modifierKeys.None), new devDept.Geometry.Point3D(-1000D, -1000D, -1000D), new devDept.Geometry.Point3D(1000D, 1000D, 1000D), 8D, 50D, 50D);
+            devDept.Eyeshot.Viewport.SavedViewsManager savedViewsManager1 = new devDept.Eyeshot.Viewport.SavedViewsManager(8);
+            devDept.Eyeshot.Viewport viewport1 = new devDept.Eyeshot.Viewport(new System.Drawing.Point(0, 0), new System.Drawing.Size(1237, 726), backgroundSettings1, camera1, new devDept.Eyeshot.ToolBar[] {
+            toolBar1}, devDept.Eyeshot.displayType.Rendered, true, false, false, false, new devDept.Eyeshot.Grid[] {
+            grid1}, false, rotateSettings1, zoomSettings1, panSettings1, navigationSettings1, savedViewsManager1, devDept.Eyeshot.viewType.Trimetric);
+            devDept.Eyeshot.CoordinateSystemIcon coordinateSystemIcon1 = new devDept.Eyeshot.CoordinateSystemIcon(System.Drawing.Color.Black, System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80))))), System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80))))), System.Drawing.Color.OrangeRed, "Origin", "X", "Y", "Z", true, devDept.Eyeshot.coordinateSystemPositionType.BottomLeft, 37, false);
+            devDept.Eyeshot.OriginSymbol originSymbol1 = new devDept.Eyeshot.OriginSymbol(10, devDept.Eyeshot.originSymbolStyleType.Ball, System.Drawing.Color.Black, System.Drawing.Color.Red, System.Drawing.Color.Green, System.Drawing.Color.Blue, "Origin", "X", "Y", "Z", true, null, false);
+            devDept.Eyeshot.ViewCubeIcon viewCubeIcon1 = new devDept.Eyeshot.ViewCubeIcon(devDept.Eyeshot.coordinateSystemPositionType.TopRight, true, System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(20)))), ((int)(((byte)(147))))), true, "FRONT", "BACK", "LEFT", "RIGHT", "TOP", "BOTTOM", System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77))))), System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77))))), System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77))))), System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77))))), System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77))))), System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77))))), 'S', 'N', 'W', 'E', true, System.Drawing.Color.White, System.Drawing.Color.Black, 120, true, true, null, null, null, null, null, null, false);
+            this.viewportLayout1 = new devDept.Eyeshot.ViewportLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.viewportLayout1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // viewportLayout1
+            // 
+            this.viewportLayout1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.viewportLayout1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.viewportLayout1.Location = new System.Drawing.Point(0, 0);
+            this.viewportLayout1.Name = "viewportLayout1";
+            this.viewportLayout1.ProgressBar = progressBar1;
+            this.viewportLayout1.Size = new System.Drawing.Size(1237, 726);
+            this.viewportLayout1.TabIndex = 0;
+            this.viewportLayout1.Text = "viewportLayout1";
+            coordinateSystemIcon1.LabelFont = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            viewport1.CoordinateSystemIcon = coordinateSystemIcon1;
+            viewport1.Legends = new devDept.Eyeshot.Legend[0];
+            originSymbol1.LabelFont = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            viewport1.OriginSymbol = originSymbol1;
+            viewCubeIcon1.Font = null;
+            viewCubeIcon1.InitialRotation = new devDept.Geometry.Quaternion(0D, 0D, 0D, 1D);
+            viewport1.ViewCubeIcon = viewCubeIcon1;
+            this.viewportLayout1.Viewports.Add(viewport1);
+            this.viewportLayout1.WriteDepthForTransparents = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1237, 726);
+            this.Controls.Add(this.viewportLayout1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.viewportLayout1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private devDept.Eyeshot.ViewportLayout viewportLayout1;
+    }
+}
+
---
+++ ChairNtable/Form1.cs
@@ -0,0 +1,163 @@
+﻿using devDept.Eyeshot;
+using devDept.Eyeshot.Entities;
+using devDept.Geometry;
+using devDept.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ChairNtable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // viewportLayout1.Unlock("");
+        }
+     
+        protected override void OnLoad(EventArgs e)
+        {
+            
+            Mesh shortleg1 = Mesh.CreateBox(10, 10, 50);
+            shortleg1.Translate(-30, 40, 0);
+            Mesh shortleg2 = (Mesh)shortleg1.Clone();
+            shortleg2.Translate(50, 0, 0);
+
+            Mesh leg1 = Mesh.CreateBox(10, 10, 120);
+            leg1.Translate(-30, 90, 0);
+            Mesh leg2 = (Mesh)leg1.Clone();
+            leg2.Translate(50, 0, 0);
+
+            Mesh cylender = Mesh.CreateSphere(10, 10, 200);
+            cylender.Translate(0, -30, 70);
+            viewportLayout1.Entities.AddRange(new Entity[] { shortleg1, shortleg2, leg1, leg2, cylender }, 0, Color.Brown);
+            shortleg1 = Mesh.CreateBox(10, 10, 50);
+            shortleg1.Translate(-80, -80, 0);
+            shortleg2 = (Mesh)shortleg1.Clone();
+            shortleg2.Translate(150, 0, 0);
+                
+            viewportLayout1.Entities.AddRange(new Entity[] { shortleg1, shortleg2 }, 0, Color.Brown);
+            shortleg1 = Mesh.CreateBox(10, 10, 50);
+            shortleg1.Translate(-80, 0, 0);
+            shortleg2 = (Mesh)shortleg1.Clone();
+            shortleg2.Translate(150, 0, 0);
+
+
+            viewportLayout1.Entities.AddRange(new Entity[] { shortleg1, shortleg2 }, 0, Color.Brown);
+
+            Mesh seatBack = Mesh.CreateBox(70, 5, 20);
+            seatBack.Translate(-35, 90, 120);
+            viewportLayout1.Entities.Add(seatBack, 0, Color.Brown);
+
+            string concreteMatName = "Concrete";
+            Block b = new Block();
+
+            double width = 70;
+            double depth = 5;
+            double dimA = 1000;
+            double dimB = 800;
+            Point2D[] outerPoints = new Point2D[7];
+
+            outerPoints[0] = new Point2D(0, dimB);
+            outerPoints[1] = new Point2D(dimA, dimB);
+            outerPoints[2] = new Point2D(dimA, 0);
+            outerPoints[3] = new Point2D(width, 0);
+            outerPoints[4] = new Point2D(width, depth);
+            outerPoints[5] = new Point2D(0, depth);
+            outerPoints[6] = (Point2D)
+                outerPoints[0].Clone();
+
+            LinearPath outer = new LinearPath(Plane.XY, outerPoints);
+
+            Point2D[] innerPoints = new Point2D[5];
+
+            innerPoints[0] = new Point2D(-30, 0);
+            innerPoints[1] = new Point2D(-30, 60);
+            innerPoints[2] = new Point2D(30, 60);
+            innerPoints[3] = new Point2D(30, 0);
+            innerPoints[4] = (Point2D)innerPoints[0].Clone();
+
+            LinearPath inner = new LinearPath(Plane.XY, innerPoints);
+
+            devDept.Eyeshot.Entities.Region reg = new devDept.Eyeshot.Entities.Region(outer, inner);
+
+            Mesh m2 = reg.ExtrudeAsMesh(10, 0.1, Mesh.natureType.Plain);
+
+            m2.ColorMethod = colorMethodType.byEntity;
+            m2.Color = Color.White;
+            m2.MaterialName = concreteMatName;
+
+            b.Entities.Add(m2);
+
+            viewportLayout1.Blocks.Add("floor", b);
+            for (int i = 0; i < 1; i++)
+            {
+
+                BlockReference reference = new BlockReference(0, 40, 60 , "floor", 1, 1, 1, 0);
+
+                viewportLayout1.Entities.Add(reference, 0);
+
+            }
+
+
+            ///////////////
+             concreteMatName = "Tableslab";
+             b = new Block();
+
+             width = 70;
+             depth = 5;
+             dimA = 1000;
+             dimB = 800;
+             outerPoints = new Point2D[7];
+
+            outerPoints[0] = new Point2D(0, dimB);
+            outerPoints[1] = new Point2D(dimA, dimB);
+            outerPoints[2] = new Point2D(dimA, 0);
+            outerPoints[3] = new Point2D(width, 0);
+            outerPoints[4] = new Point2D(width, depth);
+            outerPoints[5] = new Point2D(0, depth);
+            outerPoints[6] = (Point2D)
+                outerPoints[0].Clone();
+
+             outer = new LinearPath(Plane.XY, outerPoints);
+
+             innerPoints = new Point2D[5];
+
+            innerPoints[0] = new Point2D(-80, -80);
+            innerPoints[1] = new Point2D(80, -80);
+            innerPoints[2] = new Point2D(80, 10);
+            innerPoints[3] = new Point2D(-80, 10);
+            innerPoints[4] = (Point2D)innerPoints[0].Clone();
+
+             inner = new LinearPath(Plane.XY, innerPoints);
+
+             reg = new devDept.Eyeshot.Entities.Region(outer, inner);
+
+             m2 = reg.ExtrudeAsMesh(10, 0.1, Mesh.natureType.Plain);
+
+            m2.ColorMethod = colorMethodType.byEntity;
+            m2.Color = Color.White;
+            m2.MaterialName = concreteMatName;
+
+            b.Entities.Add(m2);
+
+            viewportLayout1.Blocks.Add("tfloor", b);
+            for (int i = 0; i < 1; i++)
+            {
+
+                BlockReference reference = new BlockReference(0, 0, 60, "tfloor", 1, 1, 1, 0);
+                
+                viewportLayout1.Entities.Add(reference, 0);
+
+            }
+            ///////////////
+
+            
+            viewportLayout1.ZoomFit();
+            //base.OnLoad(e);
+        }
+    }
+  
+}
